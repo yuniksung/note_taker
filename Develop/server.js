73d8439f2a1538261d9f2be3afe51dcb6871b80c
Maxx Sanner <maxxsanner105@gmail.com>
2020-08-23 15:21:14 -0700

    added id json object when notes are saved

--- Develop/server.js
+++ Develop/server.js
@@ -1,41 +1,63 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+// var db = require("./db/db.json");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname + "/public"));
 
 notes = [];
+
+
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./db/db.json"));
+    // fs.readFile("db/db.json", "utf8", function(error, data){
+    //     if (error) {
+    //         return console.log(error);
+    //     }
+    //     console.log(data);
+    // });
+    //return res.json()
+});
+
+let noteData = [];
+let id = 1;
+
+app.post("/api/notes", function(req, res) {
+    var newNote = req.body;
+    newNote.id = id++;
+    noteData.push(newNote);
+    console.log(noteData);
+
+    fs.writeFile("./db/db.json", JSON.stringify(noteData), function(error){
+        res.json(noteData);
+        if (error) {
+            return console.log(error);
+        }
+        console.log('SUCCESS!');
+    });
+
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    // var id = req.params.id;
+
+    // fs.writeFile("./db/db.json", JSON.stringify(db), function () {
+    //     res.json(db);
+    // });
+});
 
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
-app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "db/db.json"));
-    //return res.json()
-});
-
-app.post("/api/notes", function(req, res) {
-    var newNote = req.body;
-    console.log(newNote);
-    // notes.push(newNote);
-    // res.json(newNote);
-    // //saveNote(newNote);
-    // fs.appendFile("db.json", newNote, function(error){
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log("Success!");
-    // });
 });
 
 app.listen(PORT, function() {
