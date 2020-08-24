19c1a4ab54dc6765a3555df37b764dd68a4749a2
Maxx Sanner <maxxsanner105@gmail.com>
2020-08-23 22:34:39 -0700

    added app.delete function

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-// var db = require("./db/db.json");
+var db = require("./db/db.json");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -12,27 +12,16 @@ app.use(express.static(__dirname + "/public"));
 
 notes = [];
 
-
-
 app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./db/db.json"));
-    // fs.readFile("db/db.json", "utf8", function(error, data){
-    //     if (error) {
-    //         return console.log(error);
-    //     }
-    //     console.log(data);
-    // });
-    //return res.json()
 });
 
 let noteData = [];
-let id = 1;
 
 app.post("/api/notes", function(req, res) {
     var newNote = req.body;
-    newNote.id = id++;
     noteData.push(newNote);
-    console.log(noteData);
+    noteData.map((noteObject, i) => noteObject.id = i + 1);
 
     fs.writeFile("./db/db.json", JSON.stringify(noteData), function(error){
         res.json(noteData);
@@ -45,11 +34,23 @@ app.post("/api/notes", function(req, res) {
 });
 
 app.delete("/api/notes/:id", function (req, res) {
-    // var id = req.params.id;
-
-    // fs.writeFile("./db/db.json", JSON.stringify(db), function () {
-    //     res.json(db);
-    // });
+    id = req.params.id;
+    
+    fs.readFile("./db/db.json", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        jsonData = JSON.parse(data);
+        jsonData.splice(id - 1, 1);
+        jsonData.map((noteObject, i) => noteObject.id = i + 1);
+      
+        console.log(jsonData);
+        
+        fs.writeFile("./db/db.json", JSON.stringify(jsonData), function () {
+            res.json(jsonData);
+        });
+    });
+    
 });
 
 app.get("/notes", function(req, res) {
